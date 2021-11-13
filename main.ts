bc0f26c5934a87cac9fb44dc1c5a0ed1474d7a66
kvapehe <33888633+kvapehe@users.noreply.github.com>
2021-11-13 02:28:06 +0100

    Levert

--- main.ts
+++ main.ts
@@ -1,1 +1,35 @@
-
+/**
+ * Stein saks og papir
+ * 
+ * Med variabel 
+ * 
+ * 0 er stein
+ * 
+ * 1 er saks
+ * 
+ * 2 er papir
+ */
+input.onButtonPressed(Button.A, function () {
+    steinsakspapir = randint(0, 2)
+    for (let index = 0; index < 3; index++) {
+        basic.showIcon(IconNames.Diamond)
+        basic.showIcon(IconNames.Scissors)
+        basic.showIcon(IconNames.Square)
+    }
+    basic.showNumber(steinsakspapir)
+    basic.pause(1000)
+    if (steinsakspapir == 0) {
+        basic.showIcon(IconNames.Diamond)
+    } else if (steinsakspapir == 1) {
+        basic.showIcon(IconNames.Scissors)
+    } else if (steinsakspapir == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showString("Hva skkjedde")
+    }
+})
+let steinsakspapir = 0
+steinsakspapir = 0
+basic.forever(function () {
+	
+})
